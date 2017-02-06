0bff32e5a9afe2fe41b8890c5ea1dc754ae1d503
Erin Bergman <ebergman@pillartechnology.com>
2017-02-06 10:44:24 -0500

    Inital commit

---
+++ src/main/java/com/pillartechnology/unexpectedtiger/UnexpectedTigerApplication.java
@@ -0,0 +1,12 @@
+package com.pillartechnology.unexpectedtiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnexpectedTigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnexpectedTigerApplication.class, args);
+	}
+}
