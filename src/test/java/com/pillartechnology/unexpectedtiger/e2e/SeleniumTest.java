0f28a38c0394a71c5fb5d9a5834fadc566eba1c7
Jennifer Kron <jkron@pillartechnology.com>
2017-02-28 14:54:42 -0500

    added selenium test for adding item

--- src/test/java/com/pillartechnology/unexpectedtiger/e2e/SeleniumTest.java
+++ src/test/java/com/pillartechnology/unexpectedtiger/e2e/SeleniumTest.java
@@ -1,9 +1,12 @@
 package com.pillartechnology.unexpectedtiger.e2e;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
@@ -21,6 +24,21 @@ public class SeleniumTest {
     public void titleIsTodoList() throws InterruptedException {
         Assert.assertTrue("title should start with Todo",
                 driver.getTitle().startsWith("Todo List"));
+    }
+
+    @Test
+    public void todoIsAddedToList() throws InterruptedException {
+        WebElement input = driver.findElement(By.id("todoInput"));
+        input.sendKeys("test Todo");
+        WebElement add = driver.findElement(By.id("add"));
+        add.click();
+        WebElement todos = driver.findElement(By.id("todos"));
+        Assert.assertTrue("testTodo should be added to todos",
+                todos.getText().contains("test Todo"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
         driver.close();
         driver.quit();
     }
