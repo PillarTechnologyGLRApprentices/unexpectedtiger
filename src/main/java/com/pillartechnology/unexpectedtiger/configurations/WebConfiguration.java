09251503e135af18ec787a070e1d1306ca5898da
Jennifer Kron <jkron@pillartechnology.com>
2017-03-07 12:27:57 -0500

    attempting to create profiles

--- src/main/java/com/pillartechnology/unexpectedtiger/configurations/WebConfiguration.java
+++ src/main/java/com/pillartechnology/unexpectedtiger/configurations/WebConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("application.properties")
+@Configuration("application-production.properties")
 public class WebConfiguration {
     @Bean
     ServletRegistrationBean h2servletRegistration(){
